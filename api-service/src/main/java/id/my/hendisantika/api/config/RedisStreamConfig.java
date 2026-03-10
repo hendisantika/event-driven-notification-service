1cd23ba712ddb0554dec4450ed6a7a46e1117f2f
hendisantika <hendisantika@yahoo.co.id>
2026-03-11 06:00:07 +0700

    feat: add RedisStreamConfig🫸🌀✏️📗🐧🐳

---
+++ api-service/src/main/java/id/my/hendisantika/api/config/RedisStreamConfig.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : event-driven-notification-service
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/26
+ * Time: 05.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RedisStreamConfig {
+
+    @Value("${notification.stream.name:notifications:stream}")
+    private String streamName;
+
+    @Bean
+    public RedisTemplate<String, Object> redisStreamTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public String notificationStreamName() {
+        return streamName;
+    }
+}
