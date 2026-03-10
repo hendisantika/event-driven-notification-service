8613479e0e2673b9e31bf178928c80923546d845
hendisantika <hendisantika@yahoo.co.id>
2026-03-11 05:41:22 +0700

    feat: add JacksonConfig in worker-service🫸🌀✏️📗🐧🐳

---
+++ worker-service/src/main/java/id/my/hendisantika/notificationservice/worker/config/JacksonConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.notificationservice.worker.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : event-driven-notification-service
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/26
+ * Time: 05.41
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
