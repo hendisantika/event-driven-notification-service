659006787a4198b35be934fb26b8e8511d856a89
hendisantika <hendisantika@yahoo.co.id>
2026-03-11 05:33:30 +0700

    feat: add NotificationChannel Enum in shared🫸🌀✏️📗🐧🐳

---
+++ shared/src/main/java/id/my/hendisantika/notificationservice/shared/NotificationChannel.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.notificationservice.shared;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : event-driven-notification-service
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/26
+ * Time: 05.33
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationChannel {
+    EMAIL,
+    SMS
+}
